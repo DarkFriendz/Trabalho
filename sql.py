7973245a49a505583512ec96a807073787cde1ff
DarkFriendz <trader.filus001@gmail.com>
2023-02-07 17:30:44 -0300

    Database

--- sql.py
+++ sql.py
@@ -1,3 +1,7 @@
 import sqlite3
 
 con = sqlite3.connect('database/teste.db')
+
+DBW = con.cursor()
+
+DBW.execute("CREATE TABLE tarefas (Id int primary key, Title varchar(200), Description text(350), Done tinyint, Data datetime, Prazo datetime, Edit datetime)")
