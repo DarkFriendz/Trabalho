488e84f9b2d9744b614b28fa8702a4b1705ae311
DarkFriendz <trader.filus001@gmail.com>
2023-02-07 18:30:45 -0300

    Database Nova

--- sql.py
+++ sql.py
@@ -1,7 +1,19 @@
-import sqlite3
+import sqlite3 as sql
+from datetime import datetime
 
-con = sqlite3.connect('database/teste.db')
+data = datetime.now()
+
+con = sql.connect('database/Database.db')
 
 DBW = con.cursor()
 
-DBW.execute("CREATE TABLE tarefas (Id int primary key, Title varchar(200), Description text(350), Done tinyint, Data datetime, Prazo datetime, Edit datetime)")
+try:
+    DBW.execute("CREATE TABLE tarefas(Id integer PRIMARY KEY, Title varchar(200), Description text(350), Done tinyint, Data datetime, Prazo datetime, Edit datetime)")
+except:
+    pass
+    #DBW.execute("DELETE FROM tarefas WHERE Title='Hospital'")
+
+
+con.commit()
+
+con.close()
