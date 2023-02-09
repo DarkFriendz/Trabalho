c71233117974a6c443b79a65ac526f5d0fb8b806
DarkFriendz <trader.filus001@gmail.com>
2023-02-09 19:52:48 -0300

    CSS Interface

--- venv/website/static/js/template.js
+++ venv/website/static/js/template.js
@@ -1,14 +1,14 @@
 /* Style */
 const style = document.getElementById("styleTemplate")
 
-if (window.innerWidth <= 1060) {
+if (window.innerWidth <= 800) {
     style.href = "static/css/mobile.css"
 } else {
     style.href = "static/css/pc.css"
 }
 
 addEventListener("resize", function() {
-    if (window.innerWidth <= 1060) {
+    if (window.innerWidth <= 800) {
         style.href = "static/css/mobile.css"
     } else {
         style.href = "static/css/pc.css"
